d53dd12e078dd585d8a08135b2adf25795075e43
gspivey <gs@gspivey.com>
2017-12-20 12:04:11 -0500

    Added file location and sizing capability

---
+++ volume/files.go
@@ -0,0 +1,77 @@
+package volume
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// We will use the FiveGiB limit for dynamic transfer limit
+const (
+	FiveGB  int64 = 5000000000 // Base10 - 1000
+	FiveGiB int64 = 5368709000 // Base2 - 1024
+)
+
+//IECFormat prints bytes in the International Electrotechnical Commission format
+func IECByteFormat(num_in uint64) string {
+	suffix := "B" //just assume bytes
+	num := float64(num_in)
+	units := []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
+	for _, unit := range units {
+		if num < 1024.0 {
+			return fmt.Sprintf("%3.1f%s%s", num, unit, suffix)
+		}
+		num = (num / 1024)
+	}
+	return fmt.Sprintf("%.1f%s%s", num, "Yi", suffix)
+}
+
+//SIFormat prints bytes in the International System of Units format
+func SIByteFormat(num_in uint64) string {
+	suffix := "B" //just assume bytes
+	num := float64(num_in)
+	units := []string{"", "K", "M", "G", "T", "P", "E", "Z"}
+	for _, unit := range units {
+		if num < 1000.0 {
+			return fmt.Sprintf("%3.1f%s%s", num, unit, suffix)
+		}
+		num = (num / 1000)
+	}
+	return fmt.Sprintf("%.1f%s%s", num, "Yi", suffix)
+}
+
+// Returns the names of the subdirectories (including their paths)
+// that match the specified search pattern in the specified directory.
+//func GetDirectories(root, pattern string) ([]string, error) {
+func GetFiles(root string) ([]string, error) {
+	dirs := make([]string, 0, 144)
+	return dirs, filepath.Walk(root, func(path string, fi os.FileInfo, err error) error {
+		if !fi.IsDir() {
+			dirs = append(dirs, path)
+			return nil
+		}
+
+		return nil
+	})
+
+}
+
+func GetFilesPathSize(path string) (filelist map[string]int64, totalsize int64, err error) {
+	// composite type map needs to be initialized
+	filelist = make(map[string]int64)
+	return filelist, totalsize, filepath.Walk(path, func(fpath string, fi os.FileInfo, err error) error {
+		if !fi.IsDir() {
+			currentfile, err := os.Stat(fpath)
+			if err != nil {
+				return err
+			}
+			// get the size
+			size := currentfile.Size()
+			filelist[fpath] = size
+			totalsize += size
+			return nil
+		}
+		return nil
+
+	})
+}
---
+++ volume/files_test.go
@@ -0,0 +1,20 @@
+package volume
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetFilesPathSize(t *testing.T) {
+	//create tmp dir with some files and tree of files
+	// write known bytes to each file
+	// pass tmp dir to FilesInPath
+	// verify names and size
+	//filelist, size, err := GetFilesPathSize("/home/gspivey/Documents/Development/go/src/github.com/drayage")
+	// TODO using relative path for portability until real test is complete
+	filelist, size, err := GetFilesPathSize("../../../")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Filelist: \n%v\n TotalSize %d\n Human Readable Size %s %s\n", filelist, size, IECByteFormat(uint64(size)), SIByteFormat(uint64(size)))
+}
